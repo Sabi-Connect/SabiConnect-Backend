76a5900b6900a41caa55e855fb26142f237e0184
MeshackYaro <yaromeshack@gmail.com>
2024-08-05 19:26:47 +0100

    added appointment model

---
+++ src/test/java/com/skilledservice/ClientService/services/ReviewTest.java
@@ -0,0 +1,14 @@
+package com.skilledservice.ClientService.services;
+
+import com.skilledservice.ClientService.dto.request.PostReviewRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReviewTest {
+    @Test
+    public void postReviewAboutSkilledWorkerTest() {
+        PostReviewRequest postReview = new PostReviewRequest();
+
+    }
+}
