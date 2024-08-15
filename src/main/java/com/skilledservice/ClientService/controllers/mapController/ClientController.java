8300f4d0f1d3068c54d012552a7cf1cd207884eb
FITZGERALD <151364513+fitzejidike@users.noreply.github.com>
2024-08-15 12:00:01 +0100

    wrote exception handlers

--- src/main/java/com/skilledservice/ClientService/controllers/mapController/ClientController.java
+++ src/main/java/com/skilledservice/ClientService/controllers/mapController/ClientController.java
@@ -1,4 +1,19 @@
 package com.skilledservice.ClientService.controllers.mapController;
 
+import com.skilledservice.ClientService.services.ServiceUtils.ClientService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/client")
+@AllArgsConstructor
 public class ClientController {
+
+    private final ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<?>
 }
